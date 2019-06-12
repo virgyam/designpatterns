efbaa1e7cb1b2df35861afa73eadb9854350e9e8
virgyam <virginia.araujo@ceiba.com.co>
2019-06-12 11:31:29 -0500

    Inicializando repositorio

---
+++ patternExamples/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Virginia
+ *
+ */
+module patternExamples {
+}
---
+++ patternExamples/src/patternExamples/creationals/builder/Demo.java
@@ -0,0 +1,24 @@
+package patternExamples.creationals.builder;
+
+public class Demo {
+	
+	public static void main(String args[]) {
+		String hello = "hello";
+		System.out.println("<p>" +hello +"</p>");
+		
+		String words[] = {"hello","world"};
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append("<ul>\n");
+		
+		for(String word:words) {
+			sb.append(String.format(" <li>%s</li>\n", word));
+		}
+		sb.append("</ul>");
+		
+		System.out.println(sb);
+
+
+	}
+
+}
